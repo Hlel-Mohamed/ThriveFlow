4c390ee6420dece504b48fb39d65846c9d2ec7eb
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2024-05-10 17:03:42 +0100

    Bottom Navigation Bar

---
+++ app/src/main/java/de/tekup/thriveflow/MainActivity.java
@@ -0,0 +1,40 @@
+package de.tekup.thriveflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import de.tekup.thriveflow.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        loadFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                loadFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.todoList) {
+                loadFragment(new TodoListFragment());
+            } else if (item.getItemId() == R.id.prayers) {
+                loadFragment(new PrayersFragment());
+            }
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
+    }
+}
